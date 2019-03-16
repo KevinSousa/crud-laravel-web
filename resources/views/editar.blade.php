0c70191b27f77df820d068cfc29c5ee7e22df03e
KevinSousa <kevinsmoura@hotmail.com>
2019-03-16 15:21:57 -0300

    Adicionando Layout

--- resources/views/editar.blade.php
+++ resources/views/editar.blade.php
@@ -1,23 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> EDITAR </title>
+	<title></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
 </head>
+<style type="text/css">
+	@import url('https://fonts.googleapis.com/css?family=Varela+Round');
+	body {
+		padding-top: 50px; 
+		font-family: 'Varela Round', sans-serif;
+	}
+
+	form{
+		margin-top: 20px;
+	}
+</style>
 <body>
+<div class="ui container">
 <div>
 	<h1> EDITAR CADASTRO </h1>
 	<p> Pagina de Edição de Cadastro </p>
 </div>
+<hr>
 <div>
-	<form action="{{route('atualizar', $usuario->id)}}" method="post">
+	<form action="{{route('atualizar', $usuario->id)}}" method="post" class="ui form">
 		{{ csrf_field() }}
 		<input type="hidden" name="_method" value="put">
 		<label> NOME: </label>
 		<input type="text" required="" name="nome" value="{{isset($usuario->nome) ? $usuario->nome : ''}}"><br><br>
 		<label> CPF: </label>
 		<input type="text" required="" name="cpf" value="{{isset($usuario->cpf) ? $usuario->cpf : ''}}"><br><br>
-		<input type="submit" value="SUBMIT">
+		<div class="ui buttons">
+			<input type="reset" name="limpar" value="Limpar" class="ui button">
+						  <div class="or"></div>
+			<input type="submit" name="cancelar" value="Cadastrar" class="ui positive button">
+		</div>
 	</form>	
+	<br><br>
+<div>
+	<a href="{{route('home')}}"><button class="ui button"> Voltar </button></a>
+</div>
+</div>
 </div>
 </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title> ADMIN </title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
+	<link rel="shortcut icon" href="icon/icon.ico" >
 </head>
+<style type="text/css">
+	@import url('https://fonts.googleapis.com/css?family=Varela+Round');
+	body {
+		padding-top: 50px; 
+		margin-bottom: 100px;
+		font-family: 'Varela Round', sans-serif;
+	}
+</style>
 <body>
+<div class="ui container">
 <h1> CADASTRO </h1>
-<form action="{{route('salvar')}}" method="post">
+<p> Área de Cadastro dos Funcionários </p>
+<hr>
+<form action="{{route('salvar')}}" method="post" class="ui form">
 	{{ csrf_field() }}
 	<label> NOME: </label>
 	<input type="text" required="" name="nome" value="{{isset($user->nome) ? $user->nome : ''}}"><br><br>
 	<label> CPF: </label>
 	<input type="text" required="" name="cpf" value="{{isset($user->cpf) ? $user->cpf : ''}}"><br><br>
-	<input type="submit" value="SUBMIT">
+	<div class="ui buttons">
+		<input type="reset" name="limpar" value="Limpar" class="ui button">
+					  <div class="or"></div>
+		<input type="submit" name="cancelar" value="Cadastrar" class="ui positive button">
+	</div>
 </form>	
+
 <h1> LISTA DE FUNCIONARIOS </h1>
-<table>
-	<tr>
+<p> Lista dos Funcionários cadastrados</p>
+<hr>
+<table class="ui celled table">
+	<tr align="center">
 		<th> ID </th>
 		<th>  NOME </th>
 		<th> CPF </th>
@@ -23,13 +43,14 @@
 	</tr>
 
 	@foreach ($users as $user)
-		<tr>
+		<tr align="center">
 			<td> {{ $user -> id }} </td>
 			<td> {{ $user -> nome }} </td>
 			<td> {{ $user -> cpf }} </td>
-			<td> <a href="{{route('remover', $user->id)}}"> DELETAR </a> <a href="{{route('editar', $user->id)}}"> EDITAR </a></td>
+			<td> <a href="{{route('remover', $user->id)}}"><i class="times icon"></i></a><a href="{{route('editar', $user->id)}}"><i class="pencil alternate icon"></i></a></td>
 		</tr>
 	@endforeach
 </table>
+</div>
 </body>
 </html>
